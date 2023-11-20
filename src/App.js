17ace248355bde6cf3bd0950f4f4a909f6c3fb30
Nursultan <tupoukidala77@gmail.com>
2023-11-20 21:38:56 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Img from "./components/Img";
+import Price from "./components/Price";
+import Text from "./components/Text";
+import Otziv from "./components/Otziv";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+const App = () => {
+    return (
+       <div className='container'>
+           <div className="row">
+               <div className="col-6">
+                   <Img/>
+               </div>
+               <div className="col-6">
+                   <Text/>
+                   <Price/>
+                   <Otziv/>
+               </div>
+           </div>
+       </div>
+    )
 }
 
 export default App;
